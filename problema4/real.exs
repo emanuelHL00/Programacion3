12fe2a31a14430c91d52f48255f96fdd54e79d20
emanuelHL00 <emanuelhern321@gmail.com>
2026-02-19 21:43:18 -0500

    upgrade

--- problema4/real.exs
+++ problema4/real.exs
@@ -14,19 +14,6 @@ defmodule EntradaReales do
     generar_mensaje(valor_descuento, valor_final)
     |> Util.mostrar_mensaje()
 
-    defp calcular_valor_descuento(valor_producto, porcentaje_descuento) do
-      valor_producto * porcentaje_descuento
-    end
-
-    defp calcular_valor_final(valor_producto, valor_descuento) do
-    valor_producto - valor_descuento
-    end
-
-    defp generar_mensaje(valor_descuento, valor_final) do
-      valor_descuento = valor_descuento |> Float.round(1)
-      valor_final = valor_final |> Float.round(1)
-      "Valor de descuento de $#{valor_descuento} y el valor final $# {valor_final}"
-    end
 
   end
 
