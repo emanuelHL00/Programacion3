228de0a6307a76abffa5637720fc2b93c051c9eb
emanuelHL00 <emanuelhern321@gmail.com>
2026-02-24 15:00:55 -0500

    upgrade

--- ejercicio/ejercicio.exs
+++ ejercicio/ejercicio.exs
@@ -1,58 +1,52 @@
 defmodule Ejercicio do
-  alias ElixirLS.LanguageServer.Plugins.Util
-  def main do
-    nombre =
-    "ingrese el nombre de ususario: "
-    |> Util.ingresar(:texto)
 
-    edad =
-    "Ingrese la edad del usaurio: "
-    |> Util.ingresar(:entero)
+  def validar_acceso(nombre, edad, credencial_valida, intentos_fallidos) do
+    unless credencial_valida do
+      {:error, "Acceso denegado, credenciales inválidas"}
 
-    credencial_valida =
-    "Usted tiene credenciales validas(si/no): "
-    |>IO.gets()
-    |>String.trim()
-    |>String.downcase()
-    |>case do
-      "si" -> true
-      "no" -> false
-    end
+    else
+      if edad < 18 do
+        {:error, "Acceso denegado, por no ser mayor de edad"}
 
-    intentos_fallidos =
-    "Cuantos intentos de registro fallidos llevas? "
-    |>Util.ingresar(:entero)
-    |>String.trim()
-    |> String.to_integer()
-
-
-    def validar_acceso(nombre, edad, credencial_valida, intentos_fallidos) do
-      unless credencial_valida do
-        {:error, "Acceso denegado, credenciales denegadas"}
+      else
+        if intentos_fallidos >= 3 do
+          {:error, "Acceso denegado, número de intentos excedidos, cuenta bloqueada"}
 
         else
-          if edad < 18 do
-           {:error, "Acceso denegado, por no ser mayor de edad"}
-
-          else
-            if intentos_fallidos >= 3 do
-          {:error, "Acceso denegado, numero de intentos excedidos, cuenta bloqueada"}
-
-            else
-              {:ok, "acceso aprobado"}
-
-
-
-          end
+          {:ok, "Acceso aprobado"}
         end
       end
+    end
   end
 
-  resultado = validar_acceso(nombre, edad, credenciales_validas, intentos_fallidos)
-  IO.inspect(resultado)
-  
+  def main do
+    nombre =
+      "Ingrese el nombre de usuario: "
+      |> Util.ingresar(:texto)
+
+    edad =
+      "Ingrese la edad del usuario: "
+      |> Util.ingresar(:entero)
+
+    credencial_valida =
+      IO.gets("¿Usted tiene credenciales válidas? (si/no): ")
+      |> String.trim()
+      |> String.downcase()
+      |> case do
+        "si" -> true
+        "no" -> false
+        _ ->
+          IO.puts("Entrada inválida, credenciales invalidas")
+          false
+      end
+
+    intentos_fallidos =
+      "¿Cuántos intentos fallidos llevas?: "
+      |> Util.ingresar(:entero)
+
+    resultado = validar_acceso(nombre, edad, credencial_valida, intentos_fallidos)
+    IO.inspect(resultado)
   end
-
-  Ejercicio.main()
-
 end
+
+Ejercicio.main()
